1a6868b7a9de31cf29da045c7d1f1eb6153f3e76
zhjrcc <zjrhello@gmail.com>
2025-05-01 17:47:12 +0800

    build: action修复

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    setupFiles: ['./backend/__test__/setupTest.js'],
-  },
 })
